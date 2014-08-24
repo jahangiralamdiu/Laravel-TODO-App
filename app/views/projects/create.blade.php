e2289c87ca7ee7e7a335276d053919def932bcd2
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-08-24 08:43:32 +0600

    add create project page

--- app/views/projects/create.blade.php
+++ app/views/projects/create.blade.php
@@ -1,1 +1,16 @@
-D:\TODO\app/views/projects/create.blade.php
+@section('main')
+<h2>Create Project</h2>
+
+{{ Form::model(new Project, ['route' => ['projects.store']]) }}
+@include('projects/partials/_form', ['submit_text' => 'Create Project'])
+{{ Form::close() }}
+@stop
+
+<!-- /app/views/projects/edit.blade.php -->
+@section('main')
+<h2>Edit Project</h2>
+
+{{ Form::model($project, ['method' => 'PATCH', 'route' => ['projects.update', $project->slug]]) }}
+@include('projects/partials/_form', ['submit_text' => 'Edit Project'])
+{{ Form::close() }}
+@stop
